eb579ba0f896434464315eae59b47891eb3235c7
as010101 <37323057+as010101@users.noreply.github.com>
2020-01-13 16:33:17 +0800

    Update TimerQueue.cpp

--- version-2/TimerQueue.cpp
+++ version-2/TimerQueue.cpp
@@ -9,58 +9,58 @@
 #include <string.h>
 
 
-		int createTimerfd()
-		{
-			int timerfd = ::timerfd_create(CLOCK_MONOTONIC,
-				TFD_NONBLOCK | TFD_CLOEXEC);
-			if (timerfd < 0)
-			{
-				std::cout << "Failed in timerfd_create";
-			}
-			return timerfd;
-		}
+// 		int createTimerfd()
+// 		{
+// 			int timerfd = ::timerfd_create(CLOCK_MONOTONIC,
+// 				TFD_NONBLOCK | TFD_CLOEXEC);
+// 			if (timerfd < 0)
+// 			{
+// 				std::cout << "Failed in timerfd_create";
+// 			}
+// 			return timerfd;
+// 		}
 
-		struct timespec howMuchTimeFromNow(Timestamp when)
-		{
-			int64_t microseconds = when.microSecondsSinceEpoch()
-				- Timestamp::now().microSecondsSinceEpoch();
-			if (microseconds < 100)
-			{
-				microseconds = 100;
-			}
-			struct timespec ts;
-			ts.tv_sec = static_cast<time_t>(
-				microseconds / Timestamp::kMicroSecondsPerSecond);
-			ts.tv_nsec = static_cast<long>(
-				(microseconds % Timestamp::kMicroSecondsPerSecond) * 1000);
-			return ts;
-		}
+// 		struct timespec howMuchTimeFromNow(Timestamp when)
+// 		{
+// 			int64_t microseconds = when.microSecondsSinceEpoch()
+// 				- Timestamp::now().microSecondsSinceEpoch();
+// 			if (microseconds < 100)
+// 			{
+// 				microseconds = 100;
+// 			}
+// 			struct timespec ts;
+// 			ts.tv_sec = static_cast<time_t>(
+// 				microseconds / Timestamp::kMicroSecondsPerSecond);
+// 			ts.tv_nsec = static_cast<long>(
+// 				(microseconds % Timestamp::kMicroSecondsPerSecond) * 1000);
+// 			return ts;
+// 		}
 
-		void readTimerfd(int timerfd, Timestamp now)
-		{
-			uint64_t howmany;
-			ssize_t n = ::read(timerfd, &howmany, sizeof howmany);
-			std::cout << "TimerQueue::handleRead() " << howmany << " at " << now.toString();
-			if (n != sizeof howmany)
-			{
-				std::cout << "TimerQueue::handleRead() reads " << n << " bytes instead of 8";
-			}
-		}
+// 		void readTimerfd(int timerfd, Timestamp now)
+// 		{
+// 			uint64_t howmany;
+// 			ssize_t n = ::read(timerfd, &howmany, sizeof howmany);
+// 			std::cout << "TimerQueue::handleRead() " << howmany << " at " << now.toString();
+// 			if (n != sizeof howmany)
+// 			{
+// 				std::cout << "TimerQueue::handleRead() reads " << n << " bytes instead of 8";
+// 			}
+// 		}
 
-		void resetTimerfd(int timerfd, Timestamp expiration)
-		{
-			// wake up loop by timerfd_settime()
-			struct itimerspec newValue;
-			struct itimerspec oldValue;
-			bzero(&newValue, sizeof newValue);
-			bzero(&oldValue, sizeof oldValue);
-			newValue.it_value = howMuchTimeFromNow(expiration);
-			int ret = ::timerfd_settime(timerfd, 0, &newValue, &oldValue);
-			if (ret)
-			{
-				std::cout << "timerfd_settime()";
-			}
-		}
+// 		void resetTimerfd(int timerfd, Timestamp expiration)
+// 		{
+// 			// wake up loop by timerfd_settime()
+// 			struct itimerspec newValue;
+// 			struct itimerspec oldValue;
+// 			bzero(&newValue, sizeof newValue);
+// 			bzero(&oldValue, sizeof oldValue);
+// 			newValue.it_value = howMuchTimeFromNow(expiration);
+// 			int ret = ::timerfd_settime(timerfd, 0, &newValue, &oldValue);
+// 			if (ret)
+// 			{
+// 				std::cout << "timerfd_settime()";
+// 			}
+// 		}
 
 
 
